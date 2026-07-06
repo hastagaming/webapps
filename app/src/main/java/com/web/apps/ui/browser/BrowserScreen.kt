bf3132dc22bdcbb87678f6e9f2360f788e9a3cb1
hastagaming <rip.hastagaming@gmail.com>
2026-07-06 11:36:46 +0700

    add one import

--- app/src/main/java/com/web/apps/ui/browser/BrowserScreen.kt
+++ app/src/main/java/com/web/apps/ui/browser/BrowserScreen.kt
@@ -1,6 +1,7 @@
 package com.web.apps.ui.browser
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
