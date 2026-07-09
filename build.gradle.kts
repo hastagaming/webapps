808e51ba24fae5e1e5f2b8332e2694662903eac8
hastagaming <rip.hastagaming@gmail.com>
2026-07-09 12:22:02 +0700

    Fix build.gradle.kts in root repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,4 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
 }
